2f4503b2d52f6450278e2334565ed3ac32484a4c
Filip Sarkowicz <filip.s@elte-s.com>
2024-05-30 13:04:02 +0200

    sqlconfig for appdbcontext

--- FantasyGame/DB/AppDbContext.cs
+++ FantasyGame/DB/AppDbContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FantasyGame.Configs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace FantasyGame.DB;
 
 public class AppDbContext : DbContext
 {
+    private readonly SqlConfig _sqlConfig;
 
+    public AppDbContext(IOptions<SqlConfig> sqlConfig)
+    {
+        _sqlConfig = sqlConfig.Value;
+    }
 }
