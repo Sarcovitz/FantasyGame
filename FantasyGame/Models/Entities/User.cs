1a34122e57d0f380fc7a4d406d5bf3e10c047c60
Filip Sarkowicz <filip.s@elte-s.com>
2024-05-30 14:51:59 +0200

    User class

---
+++ FantasyGame/Models/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FantasyGame.Models.Entities;
+
+public class User
+{
+    [Required]
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public ulong Id { get; set; }
+    [Required]
+    [StringLength(16)]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [StringLength(64)]
+    public string PasswordHash { get; set; } = string.Empty;
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public bool IsEmailConfirmed { get; set; } = false;
+}
