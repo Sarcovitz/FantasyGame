bf25e327683fa9f15893fbda1f65560fdf76458f
Filip Sarkowicz <filip.s@elte-s.com>
2024-05-30 13:20:09 +0200

    database initialization

---
+++ FantasyGame/Extensions/AppDbContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FantasyGame.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasyGame.Extensions;
+
+public static class AppDbContextExtensions
+{
+    public static bool InitializeDatabase(this AppDbContext context)
+    {
+        try
+        {
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+
+            return true;
+        }
+        catch
+        {
+            //TODO log
+            return false;
+        }
+    }
+}
--- FantasyGame/Program.cs
+++ FantasyGame/Program.cs
@@ -1,13 +1,15 @@
 using FantasyGame.Configs;
 using FantasyGame.DB;
+using FantasyGame.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 if (!File.Exists("appsettings.json"))
 {
-    throw new Exception("appsettings.json file not found");
+    throw new Exception("File appsettings.json not found.");
 }
 
 //CONFIGURATION
@@ -33,6 +35,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(ServiceLifetime.Scoped);
 
 var app = builder.Build();
+
+if(!app.Services.GetRequiredService<AppDbContext>().InitializeDatabase())
+{
+    throw new Exception("Failed to initialize database.");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
