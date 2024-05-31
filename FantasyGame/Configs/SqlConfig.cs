b47aa6f9f85053636d45e573570d9981620f626d
Filip Sarkowicz <filip.s@elte-s.com>
2024-05-31 21:53:11 +0200

    SqlConfig beautified summaries

--- FantasyGame/Configs/SqlConfig.cs
+++ FantasyGame/Configs/SqlConfig.cs
@@ -1,12 +1,12 @@
 ﻿namespace FantasyGame.Configs;
 
 /// <summary>
-/// Represents data necessary to configure connection to SQL database
+///     Represents data necessary to configure connection to SQL database
 /// </summary>
 public class SqlConfig
 {
     /// <summary>
-    /// Gets or sets connection string to SQL database
+    ///     Gets or sets connection string to SQL database
     /// </summary>
     public string ConnectionString { get; } = string.Empty;
 }
