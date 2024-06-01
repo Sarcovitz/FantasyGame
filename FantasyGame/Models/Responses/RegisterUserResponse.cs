728d9d28417507d1cdd9e4f9e231cf5853a09fbe
Filip Sarkowicz <filip.s@elte-s.com>
2024-06-01 12:38:24 +0200

    RegisterUserResponse class

---
+++ FantasyGame/Models/Responses/RegisterUserResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace FantasyGame.Models.Responses;
+
+/// <summary>
+///     Represents response for /auth/register endpoint.
+/// </summary>
+public class RegisterUserResponse
+{
+    /// <summary>
+    ///     Gets or sets Id.
+    /// </summary>
+    public ulong Id { get; set; } = 0;
+    /// <summary>
+    ///     Gets or sets Username.
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+    /// <summary>
+    ///     Gets or sets Email.
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+}
