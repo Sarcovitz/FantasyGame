ea94281c5ac63852a31a9ae4cec61d8ee2664823
Filip Sarkowicz <85794698+Sarcovitz@users.noreply.github.com>
2024-06-02 08:41:08 +0000

    ExceptionHandlingMiddleware simple implementation

--- FantasyGame/Middlewares/ExceptionHandlingMiddleware.cs
+++ FantasyGame/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,6 +1,19 @@
+using System.Net;
+
 namespace FantasyGame.Models.Middlewares;
 
-public class ExceptionHandlingMiddleware
+public class ExceptionHandlingMiddleware : IMiddleware
 {
-
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try 
+        { 
+            await next(context); 
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(ex.Message);
+        }
+    }
 }
