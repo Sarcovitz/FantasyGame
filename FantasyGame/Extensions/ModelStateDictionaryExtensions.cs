bc692b15669b5148920b83dbfa9df15280112e88
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-05 17:52:09 +0200

    ModelStateDictionaryExtensions GetErrors enchanced

--- FantasyGame/Extensions/ModelStateDictionaryExtensions.cs
+++ FantasyGame/Extensions/ModelStateDictionaryExtensions.cs
@@ -8,20 +8,19 @@ namespace FantasyGame.Extensions;
 public static class ModelStateDictionaryExtensions
 {
     /// <summary>
-    ///     Extension method that gets string of <see cref="ModelStateDictionary"/> errors separated by <see cref="Environment.NewLine"/>
+    ///     Extension method that gets list of <see cref="ModelStateDictionary"/> errors.
     /// </summary>
     /// <param name="modelState">
     ///     Extended object
     /// </param>
-    /// <returns></returns>
-    public static string GetErrors(this ModelStateDictionary modelState)
+    /// <returns>
+    ///     A list of errors.
+    /// </returns>
+    public static List<string> GetErrors(this ModelStateDictionary modelState)
     {
-        string result = string.Empty;
-        List<string> values = modelState.Values
+        return modelState.Values
             .SelectMany(v => v.Errors)
             .Select(v => v.ErrorMessage)
             .ToList();
-        values.ForEach(v => result += v + Environment.NewLine);
-        return result.TrimEnd();
     }
 }
