5308b5fd4dfaa5522a18d6f1b886307d61e407aa
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-06 23:04:24 +0200

    extended LogEntry model

--- FantasyGame/Models/Entities/LogEntry.cs
+++ FantasyGame/Models/Entities/LogEntry.cs
@@ -13,5 +13,11 @@ public class LogEntry
     [Required]
     public LogSeverity Severity { get; set; } = LogSeverity.NONE;
     [Required]
+    public string File { get; set; } = string.Empty;
+    [Required]
+    public string Method { get; set; } = string.Empty;
+    [Required]
+    public int Line { get; set; } = 0;
+    [Required]
     public string Message { get; set; }  = string.Empty;
 }
