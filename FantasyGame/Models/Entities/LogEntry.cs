6a9a092fd7acca92be5b7cde7d31922ae6a3ffb7
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-06 20:40:09 +0200

    LogEntry model

---
+++ FantasyGame/Models/Entities/LogEntry.cs
@@ -0,0 +1,11 @@
+﻿using FantasyGame.Enums;
+
+namespace FantasyGame.Models.Entities;
+
+public class LogEntry
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+    public LogSeverity Severity { get; set; } = LogSeverity.NONE;
+    public string Message { get; set; }  = string.Empty;
+}
