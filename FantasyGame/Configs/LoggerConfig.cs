315f2a5a8c1dca18578fc6026d074c3372b3e825
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-06 15:46:45 +0200

    LoggerConfig subclasses

--- FantasyGame/Configs/LoggerConfig.cs
+++ FantasyGame/Configs/LoggerConfig.cs
@@ -4,7 +4,19 @@ public class LoggerConfig
 {
     public bool UseConsoleLogger { get; set; } = false;
     public bool UseFileLogger { get; set; } = false;
+    public FileLoggerConfig? FileLoggerConfig { get; set; } = null;
     public bool UseSyslogLogger { get; set; } = false;
-    public string FileLoggerPath { get; set; } = string.Empty;
+    public SyslogLoggerConfig? SyslogLoggerConfig { get; set; } = null;
+}
 
+public class FileLoggerConfig
+{
+    public string FileLoggerPath { get; set; } = string.Empty;
+}
+
+public class SyslogLoggerConfig
+{
+    public string ServerHostname { get; set; } = string.Empty;
+    public int ServerPort { get; set; } = 0;
+    public string SyslogIdentifier { get; set; } = string.Empty;
 }
