cef947d1627760cb5e58d3f0b7e12db4910410f3
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-06 16:21:26 +0200

    configuring LoggerService

--- FantasyGame/Services/LoggerService.cs
+++ FantasyGame/Services/LoggerService.cs
@@ -1,12 +1,45 @@
-﻿using FantasyGame.Services.Interfaces;
+﻿using FantasyGame.Configs;
+using FantasyGame.Services.Interfaces;
+using Microsoft.Extensions.Options;
 using System.Runtime.CompilerServices;
 
 namespace FantasyGame.Services;
 
 public class LoggerService : ILoggerService
 {
-    public static void Info(string message, [CallerFilePath]string file = "", [CallerMemberName]string memberName = "", [CallerLineNumber]int lineNumber = 0)
+    private readonly LoggerConfig _config;
+
+    private readonly FileLoggerConfig _fileLoggerConfig = new();
+    private readonly SyslogLoggerConfig _syslogLoggerConfig = new();
+
+    public LoggerService(IOptions<LoggerConfig> config)
+    {
+        _config = config.Value;
+
+        if(_config.UseFileLogger)
+        {
+            if (_config.FileLoggerConfig is null)
+            {
+                throw new Exception("Cannot configure FileLogger - config is null.");
+            }
+
+            _fileLoggerConfig = _config.FileLoggerConfig;
+        }
+
+        if(_config.UseSyslogLogger)
+        {
+            if (_config.SyslogLoggerConfig is null)
+            {
+                throw new Exception("Cannot configure SyslogLogger - config is null.");
+            }
+
+            _syslogLoggerConfig = _config.SyslogLoggerConfig;
+        }
+    }
+
+    public void Info(string message, [CallerFilePath]string file = "", [CallerMemberName]string memberName = "", [CallerLineNumber]int lineNumber = 0)
     {
         Console.Write($"{message}, {file}, {memberName}, {lineNumber}");
+        Console.WriteLine(_fileLoggerConfig.FileLoggerPath);
     }
 }
