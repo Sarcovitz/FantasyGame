e8f85e5ad2b8ac1a22b0cb834d524aaa8f1357be
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-06 15:24:04 +0200

    LoggerConfig class

---
+++ FantasyGame/Configs/LoggerConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace FantasyGame.Configs;
+
+public class LoggerConfig
+{
+    public bool UseConsoleLogger { get; set; } = false;
+    public bool UseFileLogger { get; set; } = false;
+    public bool UseSyslogLogger { get; set; } = false;
+    public string FileLoggerPath { get; set; } = string.Empty;
+
+}
