4c3dea223397889ad609cd6b50dfcca55bca96ad
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-08 15:42:02 +0200

    CryptographyConfig class

---
+++ FantasyGame/Configs/CryptographyConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace FantasyGame.Configs;
+
+/// <summary>
+///     Represents data necessary to configure JWT Authentication
+/// </summary>
+public class CryptographyConfig
+{
+    /// <summary>
+    ///     Gets or sets JWT Secret
+    /// </summary>
+    public string AesKey { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets JWT Secret
+    /// </summary>
+    public string AesIV { get; set; } = string.Empty;
+}
