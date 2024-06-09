430c6d5fea258b08a881df60c5de2bd92f0c2022
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-09 14:22:55 +0200

    EmailConfig

--- FantasyGame/Configs/EmailConfig.cs
+++ FantasyGame/Configs/EmailConfig.cs
@@ -1,5 +1,32 @@
 ﻿namespace FantasyGame.Configs;
 
+/// <summary>
+///     Represents data necessary to configure SMTP client.
+/// </summary>
 public class EmailConfig
 {
+    /// <summary>
+    ///     Gets or sets SmtpServer.
+    /// </summary>
+    public string SmtpServer { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets SmtpPort.
+    /// </summary>-
+    public string SmtpPort { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets SmtpUsername.
+    /// </summary>
+    public string SmtpUsername { get; set;} = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets SmtpPassword.
+    /// </summary>
+    public string SmtpPassword { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets SmtpEnableSSL.
+    /// </summary>
+    public bool SmtpEnableSSL { get; set; } = false;
 }
