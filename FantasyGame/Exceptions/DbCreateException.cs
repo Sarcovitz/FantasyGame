04285993ed1d9d5fa9dcd67e5513323e6efe7291
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-09 11:33:38 +0200

    DbCreateException

---
+++ FantasyGame/Exceptions/DbCreateException.cs
@@ -0,0 +1,13 @@
+﻿namespace FantasyGame.Exceptions;
+/// <summary>
+///     Exception thrown on database CREATE opeartion failrue.
+/// </summary>
+[Serializable]
+public class DbCreateException : Exception
+{
+    public DbCreateException() { }
+
+    public DbCreateException(string message) : base(message) { }
+
+    public DbCreateException(string message, Exception innerException) : base(message, innerException) { }
+}
