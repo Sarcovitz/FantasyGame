8df8416f4d9e72b354b65243bf4b6c7b35cb147e
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-11 17:29:03 +0200

    using email template for confirmation mail

--- FantasyGame/Services/EmailService.cs
+++ FantasyGame/Services/EmailService.cs
@@ -5,6 +5,7 @@ using System.Net.Mail;
 using System.Net;
 using System.Text;
 using Microsoft.Extensions.Options;
+using System.Security.Principal;
 
 namespace FantasyGame.Services;
 
@@ -36,17 +37,30 @@ public class EmailService : IEmailService
     {
         string userDataRaw = $"{user.Username}:{user.Id}:{user.Email}";
         string userData = Convert.ToBase64String(Encoding.UTF8.GetBytes(userDataRaw));
-        string userDataUrl = WebUtility.UrlEncode(userData);
+        string userDataUrlEncoded = WebUtility.UrlEncode(userData);
+
+        string confirmationUrl = @$"{"test"}/auth/confirm-account/{userDataUrlEncoded}"; // TODO DOMAIN FROM CONFIG
+        string filePath = @"./Misc/EmailTemplates/AccountConfirmationEmail.html";
+
+        if (!File.Exists(filePath))
+        {
+            //TODO LOG
+            //TODO EXCEPTION
+            return false;
+        }
+
+        string messageContent = File.ReadAllText(filePath);
+        messageContent = messageContent.Replace("***USERNAME***", user.Username);
+        messageContent = messageContent.Replace("***LINK***", confirmationUrl);
 
         MailAddress mailFrom = new(_emailConfig.EmailAddressFrom, "FANTASY GAME");
         MailAddress mailTo = new(user.Email, user.Username.ToUpper());
-
         MailMessage msg = new(mailFrom, mailTo)
         {
             SubjectEncoding = Encoding.UTF8,
             BodyEncoding = Encoding.UTF8,
             Subject = "E-mail address confirmation.",
-            Body = $"<h1><a href=\"{"test"}/auth/confirm-account/{userDataUrl}\"> Confirm Account </a></h1>", //TODO CONTENT AND DOMAIN
+            Body = messageContent,
             IsBodyHtml = true
         };
 
