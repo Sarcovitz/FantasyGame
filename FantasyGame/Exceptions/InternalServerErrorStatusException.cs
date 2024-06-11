6100e8537435d8ddf1d33dea158b1a1d47e9ffe6
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-11 19:33:37 +0200

    InternalServerErrorStatusException summaries

--- FantasyGame/Exceptions/InternalServerErrorStatusException.cs
+++ FantasyGame/Exceptions/InternalServerErrorStatusException.cs
@@ -6,7 +6,21 @@
 [Serializable]
 public class InternalServerErrorStatusException : Exception
 {
+    /// <summary>
+    ///     Constructor for <see cref="BadRequestStatusException"/>.
+    /// </summary>
     public InternalServerErrorStatusException() { }
+
+    /// <summary>
+    ///     Constructor for <see cref="BadRequestStatusException"/>.
+    /// </summary>
+    /// <param name="message">Message of exception.</param>
     public InternalServerErrorStatusException(string message) : base(message) { }
+
+    /// <summary>
+    ///     Constructor for <see cref="BadRequestStatusException"/>.
+    /// </summary>
+    /// <param name="message">Message of exception.</param>
+    /// <param name="inner">Inner <see cref="Exception"/>.</param>
     public InternalServerErrorStatusException(string message, Exception inner) : base(message, inner) { }
 }
