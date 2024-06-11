1679394131c0c7c7111c4741e0431d617ce94be7
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-11 19:42:58 +0200

    User summaries

--- FantasyGame/Models/Entities/User.cs
+++ FantasyGame/Models/Entities/User.cs
@@ -4,26 +4,52 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FantasyGame.Models.Entities;
 
+/// <summary>
+///     Entity class representing a single record in Users table.
+/// </summary>
 public class User
 {
+    /// <summary>
+    ///     Gets or sets Id.
+    /// </summary>
     [Required]
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public ulong Id { get; set; }
+    public ulong Id { get; set; } = 0;
+
+    /// <summary>
+    ///     Gets or sets Username.
+    /// </summary>
     [Required]
     [MaxLength(16)]
     [MinLength(3)]
     public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets Email.
+    /// </summary>
     [Required]
     [EmailAddress]
     [MaxLength(320)]
     [MinLength(6)]
     public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets PasswordHash.
+    /// </summary>
     [Required]
     [MaxLength(64)]
     public string PasswordHash { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets CreatedAt.
+    /// </summary>
     [Required]
     public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    /// <summary>
+    ///     Gets or sets IsEmailConfirmed.
+    /// </summary>
     [Required]
     public bool IsEmailConfirmed { get; set; } = false;
 }
