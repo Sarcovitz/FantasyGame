f33c1fadc0e8eb913b704089f50dc977e245cbfb
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-11 18:47:30 +0200

    summaries

--- FantasyGame/Services/Interfaces/IEmailService.cs
+++ FantasyGame/Services/Interfaces/IEmailService.cs
@@ -4,5 +4,11 @@ namespace FantasyGame.Services.Interfaces;
 
 public interface IEmailService
 {
+    /// <summary>
+    ///     Register new user account.
+    /// </summary>
+    /// <param name="user"> <see cref="User"/> object to get e-mail details.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    /// <exception cref="Multiple exceptions"></exception>
     public Task SendAccountConfirmationEmailAsync(User user);
 }
