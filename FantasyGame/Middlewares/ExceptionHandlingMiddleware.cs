9db064ade81d9569409aba99fac0113ba408f852
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-11 18:31:07 +0200

    ExceptionHandlingMiddleware handling new exceptions

--- FantasyGame/Middlewares/ExceptionHandlingMiddleware.cs
+++ FantasyGame/Middlewares/ExceptionHandlingMiddleware.cs
@@ -11,7 +11,22 @@ public class ExceptionHandlingMiddleware : IMiddleware
         { 
             await next(context); 
         }
-        catch(DbCreateException ex)
+        catch (BadRequestStatusException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (ConflictStatusException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (DbCreateException ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(ex.Message);
+        }
+        catch (InternalServerErrorStatusException ex)
         {
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             await context.Response.WriteAsync(ex.Message);
