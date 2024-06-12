0cafc5f216d4ad9f8799e40e6ccbad9878bc01f5
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-12 10:59:59 +0200

    GetSHA256HashAsync summary

--- FantasyGame/Services/Interfaces/ICryptographyService.cs
+++ FantasyGame/Services/Interfaces/ICryptographyService.cs
@@ -4,5 +4,11 @@ public interface ICryptographyService
 {
     public Task<string> AesDecryptAsync(string cipherText);
     public Task<string> AesEncryptAsync(string input);
+
+    /// <summary>
+    ///     Function responsibole for getting SHA256 hash of supplied string.
+    /// </summary>
+    /// <param name="input">Input string that should be hashed.</param>
+    /// <returns>A <see cref="Task"/> with string value of hash.</returns>
     public Task<string> GetSHA256HashAsync(string input);
 }
