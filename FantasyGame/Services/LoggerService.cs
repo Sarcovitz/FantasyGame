ebab58051203baf7e91c8e9246ec37cbdf4e2ac2
Filip Sarkowicz <fsarkowicz@gmail.com>
2024-06-12 12:58:04 +0200

    LogMessage added obj to serialize parameter

--- FantasyGame/Services/LoggerService.cs
+++ FantasyGame/Services/LoggerService.cs
@@ -43,36 +43,37 @@ public class LoggerService : ILoggerService
     #region ILoggerService
 
     public void Debug(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-        => LogMessage(LogSeverity.DEBUG, message, file, method, line);
+        => LogMessage(LogSeverity.DEBUG, message, file, method, line, obj);
     public void Error(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-    => LogMessage(LogSeverity.ERROR, message, file, method, line);
+    => LogMessage(LogSeverity.ERROR, message, file, method, line, obj);
 
     public void Fatal(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-        => LogMessage(LogSeverity.FATAL, message, file, method, line);
+        => LogMessage(LogSeverity.FATAL, message, file, method, line, obj);
 
     public void Info(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-        => LogMessage(LogSeverity.INFO, message, file, method, line);
+        => LogMessage(LogSeverity.INFO, message, file, method, line, obj);
 
     public void Trace(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-        => LogMessage(LogSeverity.TRACE, message, file, method, line);
+        => LogMessage(LogSeverity.TRACE, message, file, method, line, obj);
 
     public void Warn(string message, object? obj = null, [CallerFilePath] string file = "", [CallerMemberName] string method = "", [CallerLineNumber] int line = 0)
-        => LogMessage(LogSeverity.WARN, message, file, method, line);
+        => LogMessage(LogSeverity.WARN, message, file, method, line, obj);
 
     #endregion ILoggerService
 
     #region Non-interface
-    
+
     /// <summary>
     ///     Method that is facade for logging to multiple targets.
     /// </summary>
     /// <param name="logLevel">Log severity of log message.</param>
     /// <param name="message">Text of log message.</param>
+    /// <param name="obj">Object to serialize</param>
     /// <param name="file">File where log was executed.</param>
     /// <param name="method">Method where log was executed.</param>
     /// <param name="line">Line where log was executed.</param>
     /// <exception cref="ArgumentException"></exception>
-    private void LogMessage(LogSeverity logLevel, string message, string file, string method, int line)
+    private void LogMessage(LogSeverity logLevel, string message, string file, string method, int line, object? obj = null)
     {
         if(logLevel < _config.MinimalLogLevel)
         {
